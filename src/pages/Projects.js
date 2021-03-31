366c2c179ef4c674193a9afccdfd4ca7a8dd29a4
Ernest Bieś <ernestbies@gmail.com>
2021-03-31 12:56:43 +0200

    Projects refactor.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,6 +5,29 @@ import {projects} from "../utils/projects";
 
 const Projects = () => {
 
+    const renderProjects = () => {
+        let view = [];
+
+        projects.map((el) => view.push(
+            <Carousel.Item className={'carousel-item'}>
+                <ProjectItem
+                    image={el.image}
+                    color={el.color}
+                    name={el.name}
+                    description={el.description}
+                    technologies={el.technologies}
+                    os={el.os}
+                    languages={el.languages}
+                    links={el.links}
+                    additionalData={el.additionalData}
+                    infoSize={el.infoSize}
+                />
+            </Carousel.Item>
+        ));
+
+        return view;
+    }
+
     return (
         <div id={'projects'} className={'carousel-style'}>
             <p style={{
@@ -15,164 +38,7 @@ const Projects = () => {
                 marginTop: 20
             }}>Projects</p>
             <Carousel className={'carousel'}>
-                <Carousel.Item className={'carousel-item'}>
-                    <ProjectItem
-                        image={projects.mediaBrowser.image}
-                        color={projects.mediaBrowser.color}
-                        name={projects.mediaBrowser.name}
-                        description={projects.mediaBrowser.description}
-                        technologies={projects.mediaBrowser.technologies}
-                        os={projects.mediaBrowser.os}
-                        languages={projects.mediaBrowser.languages}
-                        links={projects.mediaBrowser.links}
-                        infoSize={'72%'}
-                    />
-                </Carousel.Item>
-
-                <Carousel.Item className={'carousel-item'}>
-                    <ProjectItem
-                        image={projects.cameraFingerprint.image}
-                        color={projects.cameraFingerprint.color}
-                        name={projects.cameraFingerprint.name}
-                        description={projects.cameraFingerprint.description}
-                        technologies={projects.cameraFingerprint.technologies}
-                        os={projects.cameraFingerprint.os}
-                        additionalData={projects.cameraFingerprint.additionalData}
-                        links={projects.mediaBrowser.links}
-                        infoSize={'78%'}
-                    />
-                </Carousel.Item>
-
-
-                <Carousel.Item className={'carousel-item'}>
-                    <ProjectItem
-                        image={projects.spaceships.image}
-                        color={projects.spaceships.color}
-                        name={projects.spaceships.name}
-                        description={projects.spaceships.description}
-                        technologies={projects.spaceships.technologies}
-                        os={projects.spaceships.os}
-                        links={projects.spaceships.links}
-                        infoSize={'28%'}
-                    />
-                </Carousel.Item>
-
-                <Carousel.Item className={'carousel-item'}>
-                    <ProjectItem
-                        image={projects.apocalyptoGame.image}
-                        color={projects.apocalyptoGame.color}
-                        name={projects.apocalyptoGame.name}
-                        description={projects.apocalyptoGame.description}
-                        technologies={projects.apocalyptoGame.technologies}
-                        os={projects.apocalyptoGame.os}
-                        links={projects.apocalyptoGame.links}
-                        infoSize={'24%'}
-                    />
-                </Carousel.Item>
-
-                <Carousel.Item className={'carousel-item'}>
-                    <ProjectItem
-                        image={projects.quizApp.image}
-                        color={projects.quizApp.color}
-                        name={projects.quizApp.name}
-                        description={projects.quizApp.description}
-                        technologies={projects.quizApp.technologies}
-                        os={projects.quizApp.os}
-                        links={projects.quizApp.links}
-                        infoSize={'28%'}
-                    />
-                </Carousel.Item>
-
-                <Carousel.Item className={'carousel-item'}>
-                    <ProjectItem
-                        image={projects.imageBinarizer.image}
-                        color={projects.imageBinarizer.color}
-                        name={projects.imageBinarizer.name}
-                        description={projects.imageBinarizer.description}
-                        technologies={projects.imageBinarizer.technologies}
-                        os={projects.imageBinarizer.os}
-                        links={projects.imageBinarizer.links}
-                        infoSize={'24%'}
-                    />
-                </Carousel.Item>
-
-                <Carousel.Item className={'carousel-item'}>
-                    <ProjectItem
-                        image={projects.calculatorApp.image}
-                        color={projects.calculatorApp.color}
-                        name={projects.calculatorApp.name}
-                        description={projects.calculatorApp.description}
-                        technologies={projects.calculatorApp.technologies}
-                        os={projects.calculatorApp.os}
-                        links={projects.calculatorApp.links}
-                        infoSize={'28%'}
-                    />
-                </Carousel.Item>
-
-                <Carousel.Item className={'carousel-item'}>
-                    <ProjectItem
-                        image={projects.databaseApp.image}
-                        color={projects.databaseApp.color}
-                        name={projects.databaseApp.name}
-                        description={projects.databaseApp.description}
-                        technologies={projects.databaseApp.technologies}
-                        os={projects.databaseApp.os}
-                        links={projects.databaseApp.links}
-                        infoSize={'24%'}
-                    />
-                </Carousel.Item>
-
-                <Carousel.Item className={'carousel-item'}>
-                    <ProjectItem
-                        image={projects.databaseAnalyzer.image}
-                        color={projects.databaseAnalyzer.color}
-                        name={projects.databaseAnalyzer.name}
-                        description={projects.databaseAnalyzer.description}
-                        technologies={projects.databaseAnalyzer.technologies}
-                        os={projects.databaseAnalyzer.os}
-                        links={projects.databaseAnalyzer.links}
-                        infoSize={'24%'}
-                    />
-                </Carousel.Item>
-
-                <Carousel.Item className={'carousel-item'}>
-                    <ProjectItem
-                        image={projects.taskPlanner.image}
-                        color={projects.taskPlanner.color}
-                        name={projects.taskPlanner.name}
-                        description={projects.taskPlanner.description}
-                        technologies={projects.taskPlanner.technologies}
-                        os={projects.taskPlanner.os}
-                        links={projects.taskPlanner.links}
-                        infoSize={'24%'}
-                    />
-                </Carousel.Item>
-
-                <Carousel.Item className={'carousel-item'}>
-                    <ProjectItem
-                        image={projects.carServiceStation.image}
-                        color={projects.carServiceStation.color}
-                        name={projects.carServiceStation.name}
-                        description={projects.carServiceStation.description}
-                        technologies={projects.carServiceStation.technologies}
-                        os={projects.carServiceStation.os}
-                        links={projects.carServiceStation.links}
-                        infoSize={'24%'}
-                    />
-                </Carousel.Item>
-
-                <Carousel.Item className={'carousel-item'}>
-                    <ProjectItem
-                        image={projects.pizzeriaDatabase.image}
-                        color={projects.pizzeriaDatabase.color}
-                        name={projects.pizzeriaDatabase.name}
-                        description={projects.pizzeriaDatabase.description}
-                        technologies={projects.pizzeriaDatabase.technologies}
-                        os={projects.pizzeriaDatabase.os}
-                        links={projects.pizzeriaDatabase.links}
-                        infoSize={'24%'}
-                    />
-                </Carousel.Item>
+                {renderProjects()}
             </Carousel>
         </div>
     );
--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,5 +1,6 @@
-export const projects = {
-    mediaBrowser: {
+export const projects = [
+    {
+        id: 0,
         name: 'Media Browser',
         description: 'System for searching information about audiovisual materials. Project implemented as part of an engineering thesis. The application allows you to search and view information about movies, series and people related to the film industry. The application is designed for devices with Android and iOS. The application supports Polish and English.',
         technologies: {
@@ -14,9 +15,11 @@ export const projects = {
         links: {
             github: 'https://github.com/ernestbies/MediaBrowser-mobile',
             youtube: 'https://youtu.be/7r1r9JiKsYc'
-        }
+        },
+        infoSize: '72%'
     },
-    cameraFingerprint: {
+    {
+        id: 1,
         name: 'Camera Fingerprint PRNU',
         description: 'Camera Fingerprint PRNU (Autopsy module) uses photo response non-uniformity of camera sensor (PRNU) to check if a photo was truly taken by a suspected camera or not. Module is not based on metadata such as EXIF, performs a physical analysis of the camera sensor, determines photo response non-uniformity (PRNU), uses the principle that each sensor pixel behaves differently with effects such as non-uniform photo response, making each sensor is unique, identifies each pixel anomaly and uses this information to create a description of the camera sensor - the camera fingerprint. The project was presented at the 2020 Open Source Digital Forensics Conference (#OSDFCon) and placed on the organizer\'s home page.',
         technologies: {
@@ -34,9 +37,11 @@ export const projects = {
         links: {
             github: 'https://github.com/ernestbies/Camera-Fingerprint-PRNU',
             youtube: 'https://www.youtube.com/watch?v=lsoqVuuq9T0'
-        }
+        },
+        infoSize: '78%'
     },
-    spaceships: {
+    {
+        id: 2,
         name: 'Spaceships',
         description: 'Spaceships is a puzzle game combining two popular games "Ships" and "Minesweeper" based on a client-server architecture. The aim of the game is to find all the ships on the board in the fewest possible steps.',
         technologies: {
@@ -46,14 +51,16 @@ export const projects = {
             database: ['PostgreSQL', 'Java Persistence API'],
             documentation: ['Microsoft Word', 'Libre Office', 'LaTeX', 'Unified Modeling Language (UML)']
         },
-        os: ['Windows','Android'],
+        os: ['Windows', 'Android'],
         color: 'purple',
         image: 'images/projects/spaceships.png',
         links: {
             github: 'https://github.com/ernestbies/SpaceShips'
-        }
+        },
+        infoSize: '28%'
     },
-    apocalyptoGame: {
+    {
+        id: 3,
         name: 'ApocalyptoGame',
         description: '"ApocalyptoGame" is a first-person shooter (FPS) game. There is one main plot of the game in the game. In the game, we play the role of a soldier set in a post-apocalyptic world. The player\'s task is to destroy the enemy ship. The player has the ability to interact with the environment, can pick up items. In order to destroy a ship, the player must collect four cells that will help him power the generator. The way to get cells is different - some of them are located on the map, some of them can be obtained by the player by killing aliens, and the last one is possible after killing the boss. After collecting all the cells, the player can power the generator on the map, which will blow up the enemy ship. The game has a developed combat and healing system.',
         technologies: {
@@ -64,9 +71,11 @@ export const projects = {
         image: 'images/projects/apocalyptogame.png',
         links: {
             github: 'https://github.com/ernestbies/ApocalyptoGame'
-        }
+        },
+        infoSize: '24%'
     },
-    quizApp: {
+    {
+        id: 4,
         name: 'Quiz app',
         description: 'Mobile application that allows you to solve on-line tests. Tests are downloaded from an external server. The user has the option of solving a specific test, has a specific time to answer a specific question. In addition, they can choose a random test and view all their and other users\' results. The application has a drawer menu that makes it easy to access all screens. The application is designed for Android and iOS.',
         technologies: {
@@ -77,9 +86,11 @@ export const projects = {
         color: 'yellow',
         links: {
             github: 'https://github.com/ernestbies/Quiz'
-        }
+        },
+        infoSize: '28%'
     },
-    imageBinarizer: {
+    {
+        id: 5,
         name: 'Image binarizer',
         description: 'The application allows you to binarize images. The application supports the basic types of binarization - low-level, high-level, two-threshold, conditional. It allows you to load any image (color or black and white), and then select a specific operation. The application works in a distributed environment using Remote Method Invocation (RMI) - a mechanism that enables remote methods of objects invocation. These objects can reside in other Java virtual machines that can be on other computers. In RMI technology, it is necessary to run three processes: client, server, register of objects.',
         technologies: {
@@ -90,22 +101,26 @@ export const projects = {
         color: 'grey',
         links: {
             github: 'https://github.com/ernestbies/ImageBinarizer'
-        }
+        },
+        infoSize: '24%'
     },
-    calculatorApp: {
+    {
+        id: 6,
         name: 'Calculator',
         description: 'App "Calculator" was created using the React Native framework. It was my first app created in React Native. The application works in landscape and potrait modes. The application has a basic set of math functions such as addition, subtraction, multiplication, division, percent. After changing the orientation, additional options are available.',
         technologies: {
             mobile: ['JavaScript', 'React Native']
         },
-        os: ['Android','iOS'],
+        os: ['Android', 'iOS'],
         color: 'blue',
         image: 'images/projects/calculator-dashboard.png',
         links: {
             github: 'https://github.com/ernestbies/CalculatorRN'
-        }
+        },
+        infoSize: '28%'
     },
-    databaseApp: {
+    {
+        id: 7,
         name: 'Database Connection App',
         description: 'The application allows you to connect to any PostgreSQL database using the JDBC engine. The login data can be saved and read because the application uses serialization. In the application, it is possible to view all tables in the database. If the table contains users\' contact details (e-mail address), it is possible to send a message. Sending e-mails is performed by Javax Mail. The user can add recipients (TO, CC, BCC), message subject and content. After approval, the e-mail is sent to the selected users.',
         technologies: {
@@ -116,9 +131,11 @@ export const projects = {
         image: 'images/projects/databaseApp.png',
         links: {
             github: 'https://github.com/ernestbies/databaseApp'
-        }
+        },
+        infoSize: '24%'
     },
-    databaseAnalyzer: {
+    {
+        id: 8,
         name: 'Database Analyzer',
         description: 'Application for analyzing database performance while using VIEW, MATERIALIZED VIEW, WITH and TEMPORARY TABLE. The application allows you to connect to any PostgreSQL database using the JDBC engine. The login data can be saved and read because the application uses serialization.',
         technologies: {
@@ -129,9 +146,11 @@ export const projects = {
         image: 'images/projects/databaseAnalyzer.png',
         links: {
             github: 'https://github.com/ernestbies/databaseAnalyzer'
-        }
+        },
+        infoSize: '24%'
     },
-    taskPlanner: {
+    {
+        id: 9,
         name: 'Task planner',
         description: 'My first school project in Angular. Task Planner covers the basics of Angular. The application includes data binding, the basic appearance of pages using HTML and CSS, forms, tables, dialogs, downloading data using the HTTP protocol from an external server and others.',
         technologies: {
@@ -142,9 +161,11 @@ export const projects = {
         image: 'images/projects/taskplanner.png',
         links: {
             github: 'https://github.com/ernestbies/task_planner'
-        }
+        },
+        infoSize: '24%'
     },
-    pizzeriaDatabase: {
+    {
+        id: 10,
         name: 'Pizzeria database',
         description: 'Pizzeria database created with Toad Data Modeler. Basic SQL components (functions, triggers, etc.) have been implemented in the project. The main goal of the project was to create a database for a pizzeria that allows the customer to place orders for the selected type of pizza along with extras, as well as after agreeing to register in the pizzeria database, getting appropriate discounts. Placing an order is possible via the Internet, by phone or in person. After confirming the payment, the preparation of the pizza is transferred for implementation. When ordering via the Internet or by phone, the customer makes payment by the pizza delivery man.',
         technologies: {
@@ -155,9 +176,11 @@ export const projects = {
         image: 'images/projects/pizzeria-database.png',
         links: {
             github: 'https://github.com/ernestbies/pizzeria-database'
-        }
+        },
+        infoSize: '24%'
     },
-    carServiceStation: {
+    {
+        id: 11,
         name: 'Car service station - BPMN',
         description: 'Project implemented as part of POWER Project (Projekt Współpraca Synergia Zarządzanie w Tarnowie). The subject of the project was to describe and model processes using Business Process Modeling Notation (BPMN) showing the operation of car service station. A detailed course of the repair process of the damaged vehicle will be presented in this project, starting from delivering damaged car for repair to handing over the vehicle. A description of the car engine repair, spare parts ordering, car handover and payment process is presented. The main goal of the project is to present the processes that work in most car service stations.',
         technologies: {
@@ -168,6 +191,7 @@ export const projects = {
         image: 'images/projects/car_service_station.png',
         links: {
             github: 'https://github.com/ernestbies/Car-service-station-BPMN'
-        }
+        },
+        infoSize: '24%'
     }
-};
+]

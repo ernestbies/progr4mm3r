eca9d38707eb474aa6ae3d1690b1e97a52256206
Ernest Bieś <ernestbies@gmail.com>
2021-04-02 12:23:07 +0200

    Added current page on navbar.

--- src/parts/Navbar.js
+++ src/parts/Navbar.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Scrollspy from 'react-scrollspy'
 
 const Navbar = () => {
-    const websiteName = 'ernestbies.com';
+
+    const WEBSITE_NAME = 'ernestbies.com';
 
     const scrollTo = (id) => {
         window.scrollTo(0, document.getElementById(id).offsetTop)
@@ -9,21 +11,24 @@ const Navbar = () => {
 
     return (
         <div className={'navbarContent'}>
-            <p key={websiteName} className={'websiteName'}>{websiteName}</p>
-            <div key={'about'} onClick={() => scrollTo('about')} className={'navbarText'}>
-                {'About'}</div>
-            <div key={'projects'} onClick={() => scrollTo('projects')} className={'navbarText'}>
-                {'Projects'}
-            </div>
-            <div key={'skills'} onClick={() => scrollTo('skills')} className={'navbarText'}>
-                {'Skills'}
-            </div>
-            <div key={'hobby'} onClick={() => scrollTo('hobby')} className={'navbarText'}>
-                {'Hobby'}
-            </div>
-            <div key={'contact'} onClick={() => scrollTo('contact')} className={'navbarText'}>
-                {'Contact'}
-            </div>
+            <p key={WEBSITE_NAME} className={'websiteName'}>{WEBSITE_NAME}</p>
+            <Scrollspy style={{display: 'inline'}} offset={-1}
+                       items={['about', 'projects', 'skills', 'hobby', 'contact']} currentClassName="is-current">
+                <div key={'about'} onClick={() => scrollTo('about')} className={'navbarText'}>
+                    {'About'}</div>
+                <div key={'projects'} onClick={() => scrollTo('projects')} className={'navbarText'}>
+                    {'Projects'}
+                </div>
+                <div key={'skills'} onClick={() => scrollTo('skills')} className={'navbarText'}>
+                    {'Skills'}
+                </div>
+                <div key={'hobby'} onClick={() => scrollTo('hobby')} className={'navbarText'}>
+                    {'Hobby'}
+                </div>
+                <div key={'contact'} onClick={() => scrollTo('contact')} className={'navbarText'}>
+                    {'Contact'}
+                </div>
+            </Scrollspy>
         </div>
     );
 }

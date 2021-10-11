f914dbaace91a91bf557ae70d5b813e55c725dba
Ernest Bieś <ernestbies@gmail.com>
2021-10-11 12:29:16 +0200

    Project item changes.

--- src/utils/contact.js
+++ src/utils/contact.js
@@ -3,7 +3,7 @@ const contact = {
     github: 'https://github.com/ernestbies',
     bitbucket: 'https://bitbucket.org/ernestbies',
     discord: 'tsl#2025',
-    linkedin: ''
+    linkedin: 'https://www.linkedin.com/in/ernestbies'
 };
 
 export default contact;
